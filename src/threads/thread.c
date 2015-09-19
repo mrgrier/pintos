f92c51ad930867a2a935bbb9cbcdbe0d70d14ef2
Montana Grier <montana.grier@gmail.com>
2015-09-19 17:27:25 -0500

    Ready list is ordered by thread priority

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -241,10 +241,14 @@ thread_unblock (struct thread *t)
 {
   enum intr_level old_level;
   ASSERT (is_thread (t));
-
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  
+  list_insert_ordered(&ready_list,
+                      &t->elem,
+                      (list_less_func*) compare_priority,
+                      NULL);
+
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -315,7 +319,12 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+  {
+    list_insert_ordered(&ready_list, 
+                        &cur->elem, 
+                        (list_less_func*) &compare_priority, 
+                        NULL);
+  }
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -579,17 +588,6 @@ allocate_tid (void)
   lock_release (&tid_lock);
 
   return tid;
-}
-
-bool 
-compare_ticks(struct list_elem* a,
-              struct list_elem* b,
-              void* aux UNUSED)
-{
-  struct thread* left = list_entry(a, struct thread, elem);
-  struct thread* right = list_entry(b, struct thread, elem);
-  
-  return left->sleep_ticks < right->sleep_ticks;
 }
 
 bool 
