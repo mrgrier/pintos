5f97ba00a6830dbeff9f8bc7e0c02e82188ad2f3
Montana Grier <montana.grier@gmail.com>
2015-09-20 18:11:30 -0500

    Implemented priority scheduling for condition variables.

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -304,7 +304,12 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+
+  list_insert_ordered(&cond->waiters,
+                      &waiter.elem,
+                      (list_less_func*) &compare_semas,
+                      NULL);
+
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
@@ -325,9 +330,16 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (!intr_context ());
   ASSERT (lock_held_by_current_thread (lock));
 
-  if (!list_empty (&cond->waiters)) 
-    sema_up (&list_entry (list_pop_front (&cond->waiters),
-                          struct semaphore_elem, elem)->semaphore);
+  if (!list_empty (&cond->waiters))
+  {
+    list_sort(&cond->waiters,
+              (list_less_func*) &compare_semas,
+              NULL);
+
+    sema_up(&list_entry(list_pop_front(&cond->waiters),
+                        struct semaphore_elem, 
+                        elem)->semaphore);
+  }
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
@@ -344,4 +356,30 @@ cond_broadcast (struct condition *cond, struct lock *lock)
 
   while (!list_empty (&cond->waiters))
     cond_signal (cond, lock);
+}
+
+bool
+compare_semas(struct list_elem *a,
+              struct list_elem *b,
+              void *aux UNUSED)
+{
+  struct semaphore_elem *left = list_entry(a, struct semaphore_elem, elem);
+  struct semaphore_elem *right = list_entry(b, struct semaphore_elem, elem);
+
+  if(list_empty(&right->semaphore.waiters)) return true;
+  if(list_empty(&left->semaphore.waiters)) return false;
+
+  struct thread *left_thread = list_entry(list_max(&left->semaphore.waiters, 
+                                                   (list_less_func*) &compare_priority,
+                                                   NULL),
+                                          struct thread,
+                                          elem);
+
+  struct thread *right_thread = list_entry(list_max(&right->semaphore.waiters,
+                                                    (list_less_func*) &compare_priority,
+                                                    NULL),
+                                           struct thread,
+                                           elem);
+
+  return left_thread->priority > right_thread->priority;
 }
--- src/threads/synch.h
+++ src/threads/synch.h
@@ -41,6 +41,8 @@ void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
 
+bool compare_semas(struct list_elem *a, struct list_elem *b, void *aux);
+
 /* Optimization barrier.
 
    The compiler will not reorder operations across an
